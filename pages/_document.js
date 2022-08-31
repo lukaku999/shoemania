94907b895f3b19349cf76b5fc22fe0bb4611782e
lukaku999 <999lukaku@gmail.com>
2022-08-31 21:16:49 +0200

    initial

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
-import React from "react";
-import Document, { Head, Html, Main, NextScript } from "next/document";
-import createEmotionServer from "@emotion/server/create-instance";
-import createCache from "@emotion/cache";
+import React from 'react';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import createEmotionServer from '@emotion/server/create-instance';
+import createCache from '@emotion/cache';
 
 export default class MyDocument extends Document {
   render() {
@@ -24,17 +24,18 @@ export default class MyDocument extends Document {
 
 MyDocument.getInitialProps = async (ctx) => {
   const originalRenderPage = ctx.renderPage;
-  const cache = createCache({ key: "css" });
+  const cache = createCache({ key: 'css' });
   const { extractCriticalToChunks } = createEmotionServer(cache);
   ctx.renderPage = () =>
     originalRenderPage({
+      // eslint-disable-next-line react/display-name
       enhanceApp: (App) => (props) => <App emotionCache={cache} {...props} />,
     });
   const initialProps = await Document.getInitialProps(ctx);
   const emotionStyles = extractCriticalToChunks(initialProps.html);
   const emotionStyleTags = emotionStyles.styles.map((style) => (
     <style
-      data-emotion={`${style.key} ${style.ids.join("")}`}
+      data-emotion={`${style.key} ${style.ids.join('')}`}
       key={style.key}
       dangerouslySetInnerHTML={{ __html: style.css }}
     />
